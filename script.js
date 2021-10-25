c88e3836efcb19074254021a53d4d7e3556800ee
terahje <terahjequinn@gmail.com>
2021-10-25 14:29:00 -0700

    added nav

--- script.js
+++ script.js
@@ -6,3 +6,23 @@
   });
 
 //  End of Tour Cards 
+
+// Navbar
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+// End of Navbar
